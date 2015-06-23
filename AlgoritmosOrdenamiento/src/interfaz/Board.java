9714d5a50b0cc3b3787b437dc3c61b820bbe8cf0
AnaTeresaQR <ana.quesada@ucrso.info>
2015-06-23 15:38:46 -0600

    cambios para pintar

--- AlgoritmosOrdenamiento/src/interfaz/Board.java
+++ AlgoritmosOrdenamiento/src/interfaz/Board.java
@@ -15,9 +15,7 @@ public class Board extends JPanel {
 
     Dibujar dibujar; // llama a la clase que se encarga de Dibujar los elementos para explicación
     int[] vector = {27, 14, 8, 19, 10, 17, 12, 11, 22, 24}; // vector que se va a ser ordenado
-    int[] vector2 = {27, 14, 8, 19, 10, 17, 7};
-    int[] vector3 = {27, 14, 8, 19, 10};
-    int[] vector4 = {27, 14, 8, 19};
+    int[][] matriz = {{27, 14, 8, 19, 10, 17, 12, 11, 22, 24}, {14, 8, 10, 12, 11, 17, 27, 19, 22, 24}, {8, 10, 11, 12, 14, 19, 27, 22, 24}, {8, 10, 12, 14, 17, 19, 22, 24, 27}};
 
     public Board() {
         dibujar = new Dibujar();
@@ -29,8 +27,19 @@ public class Board extends JPanel {
     @Override
     public void paint(Graphics g) {
         this.paintComponent(g);
-        dibujar.dibujarVector(g, vector, new int[]{7, 2, 5}, Dibujar.PRIMERA_LINEA);
-        dibujar.dibujarVectorBandera(g, 9, vector, Dibujar.TERCERA_LINEA);
+        pintarQuick(g);
+    }
+
+    public void pintarQuick(Graphics g) {
+        this.paintComponent(g);
+            int[] v1 = matriz[1];
+            int[] v2 = matriz[2];
+            int[] v3 = matriz[3];
+            int[] v4 = matriz[4];
+            dibujar.dibujarVector(g, v1, new int[]{}, Dibujar.PRIMERA_LINEA);
+            dibujar.dibujarVector(g, v2, new int[]{}, Dibujar.SEGUNDA_LINEA);
+            dibujar.dibujarVector(g, v3, new int[]{}, Dibujar.TERCERA_LINEA);
+            dibujar.dibujarVector(g, v4, new int[]{}, Dibujar.CUARTA_LINEA);
     }
 
     /**
